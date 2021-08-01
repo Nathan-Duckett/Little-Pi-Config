459ff784cd99622f66976d4bac7da8d63e73fe81
Nathan Duckett <nduckett13@gmail.com>
2021-08-01 15:57:14 +1200

    Initial Version

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+##### DEFAULTS #####
+
+####################
+
+# Set static IP configuration
+cat < EOF >> sudo tee -a /etc/dhcpd.conf
+interface wlan0
+static ip_address=192.168.254.73/24
+static routers=192.168.254.1
+static domain_name_servers=1.1.1.1
+EOF
+
+## INSTALLING APPLICATIONS ##
+
+sudo apt update && sudo apt upgrade -y
+
+# Must use the docker convenience script as that is the only one to support Raspbian
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
